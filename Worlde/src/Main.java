c9b1783bce64633cc49c374805548da2eff8d4cc
Btncr13 <betancor13@gmail.com>
2025-03-19 18:50:23 +0100

    Main.java

--- Worlde/src/Main.java
+++ Worlde/src/Main.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> palabras = WordleFileManager.cargarPalabras("palabras.txt");
-        WordleGame game = new WordleGame(palabras);
+        ArrayList<String> palabras = WordleFileManager.cargarPalabras("palabras.txt");	// Carga la lista de palabras desde un archivo
+        WordleGame game = new WordleGame(palabras);										// Crea una instancia del juego con las palabras cargadas
         
-        String[] wordsArray = palabras.toArray(new String[0]);
-        game.selectRandomWord(wordsArray); // Seleccionar la palabra secreta
-        game.start(); // Iniciar el juego
+        String[] wordsArray = palabras.toArray(new String[0]);							// Convierte la lista de palabras en un array
+        game.selectRandomWord(wordsArray); 												// Selecciona una palabra aleatoria como palabra secreta
+        game.start(); 																	// Iniciar el juego
     }
 }
