5d19d67a96c091c846e453f3562124716893f44a
Btncr13 <betancor13@gmail.com>
2025-03-19 18:49:48 +0100

    WordleFileManager.java

--- Worlde/src/WordleFileManager.java
+++ Worlde/src/WordleFileManager.java
@@ -1,5 +1,6 @@
-import java.io.*; 		// Para trabajar con archivos
-import java.util.*; 	// Para trabajar con listas (ArrayList)
+import java.io.*; 																		// Para trabajar con archivos
+import java.nio.charset.StandardCharsets;
+import java.util.*; 																	// Para trabajar con listas (ArrayList)
 
 public class WordleFileManager {
 	
@@ -7,24 +8,26 @@ public class WordleFileManager {
     	
 		ArrayList<String> palabras = new ArrayList<>();   								// Crea una lista vacía para guardar las palabras
     	
-		try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {   // abre el archivo con BufferedReader para leer línea por línea
+		try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {   // Abre el archivo con BufferedReader para leer línea por línea
         	String linea;   															// Variable para guardar cada línea del archivo
         	
         	while ((linea = br.readLine()) != null) {   								// Lee una palabra. Si no es nula, sigue leyendo
-        		palabras.add(linea.trim().toLowerCase());  								// Añade la palabra a la lista
+        		palabras.add(linea.trim().toLowerCase());  								// Añade la palabra a la lista en minúsculas y sin espacios extra
         	}
         } catch (IOException e) {   													// Si ocurre un error al leer el archivo, muestra un mensaje en la consola
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
-        return palabras;  																 // Devuelve la lista con las palabras cargadas
+        return palabras;  																// Devuelve la lista con las palabras cargadas
     }
 	
     public static void guardarPartida(String texto, String nombreArchivo) {				// Método para guardar texto en un archivo
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo, true))) {
-            writer.write(texto); 														// Escribir el texto en el archivo
-            writer.newLine();															// Añadir una nueva línea
-        } catch (IOException e) {
-            System.out.println("Error al guardar la partida: " + e.getMessage());
-        }
-    }
+    
+    	 try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(nombreArchivo, true), StandardCharsets.UTF_8))) {
+    	        String textoSinColores = texto.replaceAll("\u001B\\[[;\\d]*m", ""); 	// Elimina códigos ANSI
+    	        writer.write(textoSinColores);
+    	        writer.newLine();
+    	    } catch (IOException e) {
+    	        System.out.println("\u001B[31m❌ Error al guardar la partida: " + e.getMessage() + "\u001B[0m");
+    	    }
+    	}
 }
